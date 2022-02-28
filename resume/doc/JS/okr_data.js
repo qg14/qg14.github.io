306e97f3fa37561687eeb3c1434e1ca1a14bc0fe
Qiguo <wxhywc@163.com>
2022-02-28 11:14:50 +0800

    add resume folder
    add resume folder

---
+++ resume/doc/JS/okr_data.js
@@ -0,0 +1,39 @@
+﻿let KR = [
+    {
+        "PROGRAM_ID": "NM_20170063",
+        "Node": "2",
+    },
+    {
+        "PROGRAM_ID": "NB_20200041",
+        "Node": "3",
+    },
+    {
+        "PROGRAM_ID": "NPE039-20200237",
+        "Node": "1",
+    }
+    ,
+    {
+        "PROGRAM_ID": "NPE039-20200176",
+        "Node": "3",
+    }
+]
+
+let OKR = [
+    {
+        "PROGRAM_ID": "NM_20170063",
+        "Node": "1",
+    },
+    {
+        "PROGRAM_ID": "NB_20200041",
+        "Node": "2",
+    },
+    {
+        "PROGRAM_ID": "NPE039-20200237",
+        "Node": "3",
+    }
+    ,
+    {
+        "PROGRAM_ID": "NPE039-20200176",
+        "Node": "2",
+    }
+]
